a3e3e6de31631ffa9888aeea962451ac24fb4441
Sheelah <sheelah.mogaka@gmail.com>
2024-09-04 08:34:11 +0000

    task_2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Add the elements to the body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $('#count').html(`${count} clicks on the button`);
+};
+
+
+// Bind the debounced function to the button click
+$('button').on('click', _.debounce(updateCounter, 500));
